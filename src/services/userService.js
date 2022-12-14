b3b08de73d1e102a6d7a44568657ccca1dc58881
Giang Pham <giang.pham1610@gmail.com>
2022-12-14 14:41:23 +0100

    create API allcodes

--- src/services/userService.js
+++ src/services/userService.js
@@ -199,8 +199,31 @@ let updateUserData = (data) => {
                     errMessage: 'User has not found!'
                 })
             }
-        }catch (e) {
+        }catch(e){
             reject(e)
+        }
+    })
+}
+
+let getAllCodeService = (typeInput) => {
+    return new Promise (async(resolve, reject) => {
+        try {
+            if(!typeInput) {
+               resolve ({
+                errCode: 1,
+                errMessage: 'Missing required parameters!'
+               }) 
+            }else {
+                let res = {};   
+                let allcode = await db.Allcode.findAll({
+                  where: {type: typeInput}
+                });
+                res.errCode = 0;
+                res.data = allcode;
+                resolve(res);
+            }         
+        }catch(e){
+            reject(e);
         }
     })
 }
@@ -211,4 +234,5 @@ module.exports = {
     createNewUser: createNewUser,
     deleteUser: deleteUser,
     updateUserData: updateUserData,
+    getAllCodeService: getAllCodeService,
 }
