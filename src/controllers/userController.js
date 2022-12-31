be340f8145f1b4021e967afa1575ebacc9137437
Giang Pham <giang.pham1610@gmail.com>
2022-12-31 22:05:08 +0100

    setTimeout for getAllCode

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -65,8 +65,11 @@ let handleDeleteUser =async (req, res) => {
 
 let getAllCode = async (req, res) => {
     try {
-        let data = await userService.getAllCodeService(req.query.type);
-        return res.status(200).json(data);
+        setTimeout( async () => {
+            let data = await userService.getAllCodeService(req.query.type);
+            return res.status(200).json(data);
+        }, 3000) 
+        
     }catch(e) {e
         console.log('Get all code error: ', e)
         return res.status(200).json({
