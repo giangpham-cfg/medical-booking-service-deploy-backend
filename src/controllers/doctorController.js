3d3164335c4b9fb210b19848b000ccfeb117c164
Giang Pham <giang.pham1610@gmail.com>
2023-01-22 17:24:45 +0100

    write api get-detail-doctor-by-id

--- src/controllers/doctorController.js
+++ src/controllers/doctorController.js
@@ -42,8 +42,24 @@ let postInfoDoctor = async (req, res) => {
     }
 }
 
+let getDetailDoctorById = async (req, res) => {
+    try{
+        let info = await doctorService.getDetailDoctorById(req.query.id);
+        return res.status(200).json(
+            info
+        )
+    }catch(e){
+        console.log(e);
+        return res.status(200).json({
+            errCode: -1,
+            errMessage: 'Error from the server'
+        })
+    }
+}
+
 module.exports = {
     getTopDoctorHome: getTopDoctorHome,
     getAllDoctors: getAllDoctors,
     postInfoDoctor: postInfoDoctor,
+    getDetailDoctorById: getDetailDoctorById
 }
