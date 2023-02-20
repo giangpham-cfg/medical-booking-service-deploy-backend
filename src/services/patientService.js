10e9ad7e1563fd41b7e08255d26cf809efa63ba7
giangpham-cfg <giang.pham1610@gmail.com>
2023-02-20 14:40:30 +0100

    use nodemailer to send email of booking confirmation

--- src/services/patientService.js
+++ src/services/patientService.js
@@ -1,5 +1,6 @@
 import db from '../models/index';
 require('dotenv').config();
+import emailService from './emailService';
 
 let postBookAppointment = (data) => {
     return new Promise(async (resolve, reject) => {
@@ -10,6 +11,15 @@ let postBookAppointment = (data) => {
                     errMessage: 'Missing parameter'
                 })
             } else {
+
+                await emailService.sendSimpleEmail({
+                    receiverEmail: data.email,
+                    patientName: 'Giang Pham',
+                    time: '8:00 - 9:00 Chủ nhật 22/02/2023',
+                    doctorName: 'Ngoc Anh',
+                    redirectLink: 'https://www.youtube.com/watch?v=0GL--Adfqhc&list=PLncHg6Kn2JT6E38Z3kit9Hnif1xC_9VqI&index=97'
+                })
+
                 //upsert patient
                 let user = await db.User.findOrCreate({
                     where: { email: data.email },
