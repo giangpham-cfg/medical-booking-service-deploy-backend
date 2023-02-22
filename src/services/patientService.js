a1719aa788045a6a7ab9e85fb157bbd2163be133
giangpham-cfg <giang.pham1610@gmail.com>
2023-02-22 21:52:41 +0100

    write api verify-booking appointment

--- src/services/patientService.js
+++ src/services/patientService.js
@@ -1,6 +1,15 @@
 import db from '../models/index';
 require('dotenv').config();
 import emailService from './emailService';
+import { v4 as uuidv4 } from 'uuid';
+import { reject } from 'lodash';
+
+let buildUrlEmail = (doctorId, token) => {
+
+    let result = `${process.env.URL_REACT}/verify-booking?token=${token}&doctorId=${doctorId}`
+
+    return result;
+}
 
 let postBookAppointment = (data) => {
     return new Promise(async (resolve, reject) => {
@@ -14,13 +23,15 @@ let postBookAppointment = (data) => {
                 })
             } else {
 
+                let token = uuidv4(); // ⇨ '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d'
+
                 await emailService.sendSimpleEmail({
                     receiverEmail: data.email,
                     patientName: data.fullName,
                     time: data.timeString,
                     doctorName: data.doctorName,
                     language: data.language,
-                    redirectLink: 'https://www.youtube.com/watch?v=0GL--Adfqhc&list=PLncHg6Kn2JT6E38Z3kit9Hnif1xC_9VqI&index=97'
+                    redirectLink: buildUrlEmail(data.doctorId, token)
                 })
 
                 //upsert patient
@@ -42,6 +53,7 @@ let postBookAppointment = (data) => {
                             patientId: user[0].id,
                             date: data.date,
                             timeType: data.timeType,
+                            token: token
                         }
                     })
                 }
@@ -58,6 +70,45 @@ let postBookAppointment = (data) => {
     })
 }
 
+let postVerifyBookAppointment = (data) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            if (!data.token || !data.doctorId) {
+                resolve({
+                    errCode: 1,
+                    errMessage: 'Missing parameter'
+                })
+            } else {
+                let appointment = await db.Booking.findOne({
+                    where: {
+                        doctorId: data.doctorId,
+                        token: data.token,
+                        statusId: 'S1'
+                    },
+                    raw: false //trả ra sequelize object (true thì sẽ trả ra object của javascript) thì mới dùng đc hàm "save". Nếu muốn biết true false khác nhau như nào thì "console.log(appointment)" để check"
+                })
+
+                if (appointment) {
+                    appointment.statusId = 'S2';
+                    await appointment.save();
+                    resolve({
+                        errCode: 0,
+                        errMessage: 'Update the appointment succeed!'
+                    })
+                } else {
+                    resolve({
+                        errCode: 2,
+                        errMessage: 'Appointment has been activated or does not exist!'
+                    })
+                }
+            }
+        } catch (e) {
+            reject(e);
+        }
+    })
+}
+
 module.exports = {
-    postBookAppointment: postBookAppointment
+    postBookAppointment: postBookAppointment,
+    postVerifyBookAppointment: postVerifyBookAppointment
 }
